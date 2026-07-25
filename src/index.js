88275e082e85a8af2817c90b8cc686ae48f420c4
Shridhar Ganiger <shridharganiger61@gmail.com>
2026-07-25 18:09:40 +0530

    Util Api Code Adding

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import dotenv  from "dotenv";
 import ConnectDB from "./db/index.js";
 import dns from "dns";
+import app from "./app.js";
 
 dns.setServers(["1.1.1.1","8.8.8.8"]);
 
-dotenv.config({path:'./env'});
+dotenv.config({path:'./.env'});
 
-ConnectDB();
+ConnectDB().
+then(()=>{
+    app.listen(process.env.PORT || 7000 ,()=>{
+        console.log(`Server is running on the port of ${process.env.PORT} `)
+    })
+}).
+catch((error)=>{
+    console.error(error);
+});
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+// const asyncHandler=(func)=>(req,res,next)=>{
+//     try{
+//         await func(req,res,next)
+//     }catch(err){
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })
+
+//     }
+// }
+
+//OR
+
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+       Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+export default asyncHandler;
